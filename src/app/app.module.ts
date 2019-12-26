bb7b14eef9cfa7470b12a4ded9f01a7c6054a8a9
Carlos1512 <carlosnieto151243@gmail.com>
2019-12-26 15:46:30 +0100

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { APP_ROUTES } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+
+
+//Modulos
+import {PagesModule} from './pages/pages.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
+import {ProgressComponent} from './pages/progress/progress.component';
+import {Graficas1Component} from './pages/graficas1/graficas1.component';
+import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
+import {PagesComponent} from './pages/pages.component';
+
+const appRoutes: Routes = [
+  {path: 'login' , component: LoginComponent},
+  {path: 'register' , component: LoginComponent},
+  {path: '**' , component: NopagefoundComponent}
+
+];
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, {useHash: true });
